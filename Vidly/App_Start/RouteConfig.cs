dfa919a5851c0d81d2f5d88096a461e48c2c5bfa
Hlias Manolakos <emanolakos@iknowhow.com>
2016-12-13 14:31:34 +0200

    025

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -11,8 +11,10 @@ namespace Vidly
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
