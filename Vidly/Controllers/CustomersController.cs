ae41de2ad7c1604beb75fdec6f2d359f21ee126c
Hlias Manolakos <emanolakos@iknowhow.com>
2016-12-13 17:17:57 +0200

    section 3 ex2 completed

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,20 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        } 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             
                         if (customer == null)
                                 return HttpNotFound();
@@ -25,16 +36,7 @@ namespace Vidly.Controllers
                         return View(customer);
          }
 
-        public IEnumerable<Customer>GetCustomers()
-        {
-            return new List<Customer>
-                 {
-               new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "elias Manolakos" }
-                };
-
-
-        }
+        
 
 
     }
