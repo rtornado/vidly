8f19ce9c40cf67d8dff51d652d957bc033c40746
Hlias Manolakos <emanolakos@iknowhow.com>
2016-12-15 16:55:33 +0200

    15/12

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -20,6 +20,10 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+        public ActionResult New()
+        {
+            return View();
+        }
 
         public ViewResult Index()
         {
